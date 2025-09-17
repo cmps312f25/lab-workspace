a376052d100e76ed55c12874350ade77391d7688
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-17 15:42:12 +0300

    updated code

--- B52-Wednesday/lab 4 -Flutter Fundamentals/tip_app/lib/main.dart
+++ B52-Wednesday/lab 4 -Flutter Fundamentals/tip_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,18 +15,20 @@ class MyApp extends StatelessWidget {
       home: Container(
         color: Colors.blue,
         child: Center(
-          child: Column(
-            children: [
-              Card(
-                color: Colors.lightGreen,
-                elevation: 20,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
-                  child: Text(
-                    "This is my first apps",
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: CupertinoColors.systemYellow,
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "First",
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 20,
                       fontFamily: "Times New Roman",
                       // backgroundColor: Colors.amber[50],
                       letterSpacing: 3,
@@ -33,47 +36,53 @@ class MyApp extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
-                ),
-              ),
-              Card(
-                color: Colors.purple,
-                elevation: 20,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
-                  child: Text(
-                    "This is my first apps",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontFamily: "Times New Roman",
-                      // backgroundColor: Colors.amber[50],
-                      letterSpacing: 3,
-                      decoration: TextDecoration.none,
-                      color: Colors.white,
+                  Icon(Icons.not_accessible, size: 50, color: Colors.red),
+                  Image.network(
+                    "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
+                    width: 200,
+                    height: 200,
+                  ),
+                  Card(
+                    color: Colors.purple,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
+                      child: Text(
+                        "Second",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Times New Roman",
+                          // backgroundColor: Colors.amber[50],
+                          letterSpacing: 3,
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Card(
-                color: Colors.pink,
-                elevation: 20,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
-                  child: Text(
-                    "This is my first apps",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontFamily: "Times New Roman",
-                      // backgroundColor: Colors.amber[100],
-                      letterSpacing: 3,
-                      decoration: TextDecoration.none,
-                      color: Colors.white,
+                  Card(
+                    color: Colors.pink,
+                    elevation: 20,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18.0, 8.0, 18.0, 8.0),
+                      child: Text(
+                        "Third",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Times New Roman",
+                          // backgroundColor: Colors.amber[100],
+                          letterSpacing: 3,
+                          decoration: TextDecoration.none,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
