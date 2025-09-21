d26b17d520d7f4a80a47259218175edd924863e4
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-21 15:23:45 +0300

    UPDATED

--- B51-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
+++ B51-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _tipPercentage = 30;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -65,9 +67,28 @@ class _MyAppState extends State<MyApp> {
             "Tip Calculator",
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 20,
               fontWeight: FontWeight.bold,
               color: Colors.red,
+            ),
+          ),
+
+          Divider(),
+          SizedBox(height: 30),
+          RadioGroup<int>(
+            onChanged: (value) {
+              setState(() {
+                // TRIGGER UI REFRESH
+                _tipPercentage = value!;
+              });
+            },
+            groupValue: _tipPercentage,
+            child: Column(
+              children: [
+                RadioListTile(value: 10, title: Text("10")),
+                RadioListTile(value: 20, title: Text("20")),
+                RadioListTile(value: 30, title: Text("30")),
+              ],
             ),
           ),
         ],
