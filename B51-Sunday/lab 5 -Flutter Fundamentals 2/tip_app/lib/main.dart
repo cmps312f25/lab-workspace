8ebaa785026fb6fa33b5eab94856633aaf225c71
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-21 16:09:57 +0300

    updated code upto design

--- B51-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
+++ B51-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
@@ -5,12 +5,16 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   int _tipPercentage = 30;
+  double _billAmount = 0.0;
+  double totalBillAmount = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,60 +43,86 @@ class _MyAppState extends State<MyApp> {
     return Container(
       width: double.infinity,
       child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
-            child: Text(
-              "Tip Calculator",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0, bottom: 20.0),
+                child: Text(
+                  "Tip Calculator",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+                  onChanged: (value) {
+                    _billAmount = double.tryParse(value) ?? 0.0;
+                    calculateBill();
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Bill Amount",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Text(
+                "Tip Calculator",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              Divider(),
+              SizedBox(height: 30),
+              RadioGroup<int>(
+                onChanged: (value) {
+                  _tipPercentage = value!;
+                  calculateBill();
+                },
+                groupValue: _tipPercentage,
+                child: Column(
+                  children: [
+                    RadioListTile(value: 10, title: Text("10")),
+                    RadioListTile(value: 20, title: Text("20")),
+                    RadioListTile(value: 30, title: Text("30")),
+                  ],
+                ),
+              ),
+            ],
           ),
 
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Bill Amount",
-                border: OutlineInputBorder(),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            child: Center(
+              child: Text(
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                "Bill Amount : ${_billAmount + _billAmount * _tipPercentage / 100}",
               ),
-            ),
-          ),
-          Text(
-            "Tip Calculator",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-            ),
-          ),
-
-          Divider(),
-          SizedBox(height: 30),
-          RadioGroup<int>(
-            onChanged: (value) {
-              setState(() {
-                // TRIGGER UI REFRESH
-                _tipPercentage = value!;
-              });
-            },
-            groupValue: _tipPercentage,
-            child: Column(
-              children: [
-                RadioListTile(value: 10, title: Text("10")),
-                RadioListTile(value: 20, title: Text("20")),
-                RadioListTile(value: 30, title: Text("30")),
-              ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  void calculateBill() {
+    setState(() {
+      totalBillAmount = _billAmount + _billAmount * _tipPercentage / 100;
+    });
   }
 }
