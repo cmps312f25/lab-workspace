8b4b11c758235fcac4f4533d96e5aba69ce2a9b9
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-21 18:12:23 +0300

    updated code

--- B03-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
+++ B03-Sunday/lab 5 -Flutter Fundamentals 2/tip_app/lib/main.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: const MyApp()));
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String title = "Hello";
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(child: Text("Hello")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Tip App",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            // stretched
+            children: [
+              SizedBox(height: 40),
+              ..._tipInput(),
+              Text(
+                "Select Tip Percentage",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  List<Widget> _tipInput() {
+    return [
+      Text(
+        "Tip Calculator",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(height: 40),
+      TextField(decoration: InputDecoration(border: OutlineInputBorder())),
+      SizedBox(height: 10),
+    ];
   }
 }
