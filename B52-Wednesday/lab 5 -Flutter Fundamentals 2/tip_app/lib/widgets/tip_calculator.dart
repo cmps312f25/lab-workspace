588c006832aa97fba7693a3b13b3a664110c2e66
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-24 15:20:32 +0300

    updated

--- B52-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator.dart
+++ B52-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator.dart
@@ -15,9 +15,32 @@ class _TipCalculatorState extends State<TipCalculator> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("Tip Calculator", style: TextStyle(fontSize: 40)),
-        
-        ],
+        Text("Tip Calculator", style: TextStyle(fontSize: 20)),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Enter Amount",
+            border: OutlineInputBorder(),
+          ),
+        ),
+        Text("Select Tip Percentage", style: TextStyle(fontSize: 20)),
+        Divider(),
+        RadioGroup(
+          groupValue: 30,
+          onChanged: (value) {},
+          child: Column(
+            children: [
+              RadioListTile(value: 10, title: Text("10")),
+              RadioListTile(value: 20, title: Text("20")),
+              RadioListTile(value: 30, title: Text("30")),
+            ],
+          ),
+        ),
+        SwitchListTile(
+          value: true,
+          onChanged: (value) {},
+          title: Text("Round Tip Up"),
+        ),
+      ],
     );
   }
 }
