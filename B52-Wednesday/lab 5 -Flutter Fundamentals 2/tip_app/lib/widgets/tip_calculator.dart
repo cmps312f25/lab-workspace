8856efe8fd6b8a2dad0e9ede542c2b1cf290aea2
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-24 15:55:05 +0300

    final calc code

--- B52-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator.dart
+++ B52-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator.dart
@@ -8,25 +8,55 @@ class TipCalculator extends StatefulWidget {
 }
 
 class _TipCalculatorState extends State<TipCalculator> {
-  String title = "";
-  int counter = 0;
+  double _billAmount = 0.0;
+  int _tipPercentage = 20;
+  bool _roundUp = false;
+  double _totalAmount = 0.0;
+
+  void calculateTip() {
+    setState(() {
+      _totalAmount = _billAmount + _billAmount * (_tipPercentage / 100);
+      if (_roundUp) {
+        _totalAmount = _totalAmount.ceilToDouble();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text("Tip Calculator", style: TextStyle(fontSize: 20)),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "Enter Amount",
-            border: OutlineInputBorder(),
+        SizedBox(height: 40),
+        Text(
+          "Tip Calculator",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            onChanged: (value) {
+              _billAmount = double.tryParse(value) ?? 0.0;
+              calculateTip();
+            },
+            decoration: InputDecoration(
+              hintText: "Enter Amount",
+              border: OutlineInputBorder(),
+            ),
           ),
         ),
         Text("Select Tip Percentage", style: TextStyle(fontSize: 20)),
-        Divider(),
+        Padding(padding: const EdgeInsets.all(20.0), child: Divider()),
         RadioGroup(
-          groupValue: 30,
-          onChanged: (value) {},
+          groupValue: _tipPercentage,
+          onChanged: (value) {
+            _tipPercentage = value!;
+            calculateTip();
+          },
           child: Column(
             children: [
               RadioListTile(value: 10, title: Text("10")),
@@ -35,10 +65,38 @@ class _TipCalculatorState extends State<TipCalculator> {
             ],
           ),
         ),
-        SwitchListTile(
-          value: true,
-          onChanged: (value) {},
-          title: Text("Round Tip Up"),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SwitchListTile(
+            value: _roundUp,
+            onChanged: (value) {
+              _roundUp = value;
+              calculateTip();
+            },
+            title: Text("Round Tip Up"),
+          ),
+        ),
+        // Spacer(flex: 4),
+        // ElevatedButton(
+        //   onPressed: () {
+        //     calculateTip();
+        //   },
+        //   style: ButtonStyle(
+        //     backgroundColor: WidgetStatePropertyAll(Colors.red),
+        //   ),
+        //   child: Text("Calculate", style: TextStyle(color: Colors.white)),
+        // ),
+        Spacer(flex: 1),
+        Container(
+          width: double.infinity,
+          height: 100,
+          color: Colors.red,
+          child: Center(
+            child: Text(
+              "Total Amount : $_totalAmount",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
         ),
       ],
     );
