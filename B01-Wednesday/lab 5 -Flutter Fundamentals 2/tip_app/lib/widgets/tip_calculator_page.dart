599431dd5b91c96374b1dcfc03b39d9d77515184
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-24 10:59:31 +0300

    updated code

--- B01-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator_page.dart
+++ B01-Wednesday/lab 5 -Flutter Fundamentals 2/tip_app/lib/widgets/tip_calculator_page.dart
@@ -10,10 +10,13 @@ class TipCalculatorPage extends StatefulWidget {
 class _TipCalculatorPageState extends State<TipCalculatorPage> {
   double _billAmount = 0.0;
   double _totalBill = 0.0;
+  int _tipPercentage = 0;
+  bool _roundUp = false;
 
   void calculateTotalBill() {
     setState(() {
-      _totalBill = _billAmount;
+      _totalBill = _billAmount + _billAmount * _tipPercentage / 100;
+      if (_roundUp) _totalBill = _totalBill.ceilToDouble();
     });
   }
 
@@ -25,11 +28,45 @@ class _TipCalculatorPageState extends State<TipCalculatorPage> {
         TextField(
           onChanged: (value) {
             _billAmount = double.tryParse(value) ?? 0.0;
+            calculateTotalBill();
           },
         ),
         Text("Select Tip Percentage"),
-        Text("$_billAmount"),
-        Text("Total Amount : ${_totalBill + (_totalBill * 0.15)}"),
+        Divider(),
+
+        RadioGroup<int>(
+          onChanged: (value) {
+            _tipPercentage = value!;
+            calculateTotalBill();
+          },
+          groupValue: _tipPercentage,
+
+          child: Column(
+            children: [
+              RadioListTile(
+                value: 10,
+                title: Text("10", style: TextStyle(color: Colors.black)),
+              ),
+              RadioListTile(
+                value: 20,
+                title: Text("20", style: TextStyle(color: Colors.black)),
+              ),
+              RadioListTile(
+                value: 30,
+                title: Text("30", style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          ),
+        ),
+        SwitchListTile.adaptive(
+          value: _roundUp,
+          onChanged: (value) {
+            _roundUp = value;
+            calculateTotalBill();
+          },
+          title: Text("Round Tip"),
+        ),
+        Text("Total Amount : $_totalBill"),
       ],
     );
   }
