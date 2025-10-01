39f175102d791d605777cd3befdc04759543015b
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-01 15:17:08 +0300

    updated code for login

--- B52-Wednesday/Lab 6 - Flutter Data-Driven UI Development with Clean Architecture/campus_hub/lib/features/user_management/presentation/pages/login_page.dart
+++ B52-Wednesday/Lab 6 - Flutter Data-Driven UI Development with Clean Architecture/campus_hub/lib/features/user_management/presentation/pages/login_page.dart
@@ -36,6 +36,50 @@ class LoginPage extends StatelessWidget {
           // SizedBox(height: 20),
           Text('Tutoring Platform', style: TextStyle(color: Colors.grey[600])),
           SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Email",
+                prefixIcon: Icon(Icons.email),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 20.0,
+              right: 20.0,
+              bottom: 20.0,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+              // keyboardType: TextInputType.emailAddress,
+            ),
+          ),
+          Text("Select Your Role :  "),
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          //   child: DropdownButtonFormField<String>(
+          //     decoration: InputDecoration(
+          //       border: OutlineInputBorder(),
+          //       prefixIcon: Icon(Icons.person),
+          //     ),
+          //     items: <String>['Student', 'Tutor', 'Admin'].map((String value) {
+          //       return DropdownMenuItem<String>(
+          //         value: value,
+          //         child: Text(value),
+          //       );
+          //     }).toList(),
+          //     onChanged: (String? newValue) {},
+          //   ),
+          // ),
         ],
       ),
     );
