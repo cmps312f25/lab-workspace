c0b42aaef549fbf23793a3dd3f96252d36c4e3ff
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-12 15:16:39 +0300

    upto the provider setup

--- Lab Solutions/B51-Sunday/Lab 8 State Management/state_managment_tutorial/lib/features/home/presentation/providers/book_notifier.dart
+++ Lab Solutions/B51-Sunday/Lab 8 State Management/state_managment_tutorial/lib/features/home/presentation/providers/book_notifier.dart
@@ -1,1 +1,14 @@
-class BookNotifier {}
+// flutter pub add flutter_riverpod
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_managment_tutorial/features/home/domain/entities/book.dart';
+
+class BookNotifier extends Notifier<List<Book>> {
+  @override
+  List<Book> build() {
+    return [];
+  }
+}
+
+final bookNotifierProvider = NotifierProvider<BookNotifier, List<Book>>(
+  () => BookNotifier(),
+);
