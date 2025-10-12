b5541b15acf473ddd94243b1140398b7453be852
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-12 17:32:14 +0300

    update

--- Lab Solutions/B52-Wednesday/Lab 8 State Management/state_management_tut/lib/features/dashboard/domain/entities/book.dart
+++ Lab Solutions/B52-Wednesday/Lab 8 State Management/state_management_tut/lib/features/dashboard/domain/entities/book.dart
@@ -1,1 +1,35 @@
-class Book {}
+// "id": 1,
+//     "title": "The Great Gatsby",
+//     "author": "F. Scott Fitzgerald",
+//     "year": 1925
+class Book {
+  final int id;
+  final String title;
+  final String author;
+  final int year;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.year,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      author: json['author'] as String,
+      year: json['year'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'title': title, 'author': author, 'year': year};
+  }
+
+  @override
+  String toString() {
+    return 'Book{id: $id, title: $title, author: $author, year: $year}';
+  }
+}
