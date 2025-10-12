2ac6e8b1c27548d430a59791103e2466f0b161eb
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-12 14:33:18 +0300

    updated

---
+++ Lab Solutions/B51-Sunday/Lab 8 State Management/state_managment_tutorial/lib/features/home/data/repository/book_repo_json.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:state_managment_tutorial/features/home/domain/contract/book_repo.dart';
+import 'package:state_managment_tutorial/features/home/domain/entities/book.dart';
+
+class BookRepoJson implements BookRepo {
+  @override
+  Future<List<String>> fetchBooks() async {
+    final booksData = await rootBundle.loadString('assets/data/books.json');
+    final booksMap = await jsonDecode(booksData);
+    final booksList = booksMap.map((book) => Book.fromJson(book)).toList();
+
+      // final booksMap = await jsonDecode(booksData) as List<Book>;
+    return booksList;
+  }
+
+  @override
+  Future<String> fetchBookDetails(String title) {
+    // TODO: implement fetchBookDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>> searchBooks(String query) {
+    // TODO: implement searchBooks
+    throw UnimplementedError();
+  }
+}
