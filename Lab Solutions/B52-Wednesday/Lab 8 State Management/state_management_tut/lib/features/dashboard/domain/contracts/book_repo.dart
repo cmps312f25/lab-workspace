bbc490cdcd1d246e40c449552ccdf7dba1b68966
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-12 17:34:51 +0300

    contract

---
+++ Lab Solutions/B52-Wednesday/Lab 8 State Management/state_management_tut/lib/features/dashboard/domain/contracts/book_repo.dart
@@ -0,0 +1,9 @@
+import 'package:state_management_tut/features/dashboard/domain/entities/book.dart';
+
+abstract class BookRepository {
+  Future<List<Book>> getBooks();
+  Future<Book> getBookById(int id);
+  Future<void> addBook(Book book);
+  Future<void> updateBook(Book book);
+  Future<void> deleteBook(int id);
+}
