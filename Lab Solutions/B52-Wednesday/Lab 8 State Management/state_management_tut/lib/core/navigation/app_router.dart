cd9353813cc0cd751af5b84bbcd1dbcb18e7fab7
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-15 15:11:49 +0300

    updated router

--- Lab Solutions/B52-Wednesday/Lab 8 State Management/state_management_tut/lib/core/navigation/app_router.dart
+++ Lab Solutions/B52-Wednesday/Lab 8 State Management/state_management_tut/lib/core/navigation/app_router.dart
@@ -1,3 +1,26 @@
+import 'package:go_router/go_router.dart';
+import 'package:state_management_tut/features/dashboard/presentation/screens/dashboard_screen.dart';
+import 'package:state_management_tut/features/settings/presentations/screens/settings_screen.dart';
+
 class AppRouter {
-  
+  static final dashboard = (
+    path: "/",
+    name: "dashboard",
+    screen: DashboardScreen(),
+  );
+  static final settings = (
+    path: "/settings",
+    name: "settings",
+    screen: SettingsScreen(),
+  );
+
+  // create the router here
+
+  static final router = GoRouter(
+    initialLocation: dashboard.path,
+    routes: [
+      GoRoute(path: dashboard.path , name : dashboard.name , builder: (context, state) => dashboard.screen,),
+      GoRoute(path: settings.path , name : settings.name , builder: (context, state) => settings.screen,),
+ 
+    ]);
 }
