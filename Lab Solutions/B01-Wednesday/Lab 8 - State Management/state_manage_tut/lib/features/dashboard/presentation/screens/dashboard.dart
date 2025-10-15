f59202385feb8113b2d1b37a8969208759e2322f
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-15 11:15:55 +0300

    updated list

--- Lab Solutions/B01-Wednesday/Lab 8 - State Management/state_manage_tut/lib/features/dashboard/presentation/screens/dashboard.dart
+++ Lab Solutions/B01-Wednesday/Lab 8 - State Management/state_manage_tut/lib/features/dashboard/presentation/screens/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:state_manage_tut/core/navigation/app_router.dart';
+import 'package:state_manage_tut/features/dashboard/domain/entities/book.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -12,6 +13,22 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Text("It is working");
+    List<String> items = ["Skating 1", "Item 2", "Item 3", "Item4"];
+    return myList(items);
+  }
+
+  Widget myList(List<Book> books) {
+    return ListView.builder(
+      itemBuilder: (context, index) => Card(
+        margin: EdgeInsets.all(10),
+        child: ListTile(
+          title: Text("$index: ${books[index].title}"),
+          subtitle: Text(books[index].author),
+          leading: Icon(Icons.book),
+          trailing: Text(books[index].year.toString()),
+        ),
+      ),
+      itemCount: books.length,
+    );
   }
 }
