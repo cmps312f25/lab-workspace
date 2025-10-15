e919dd88a5b8023dc8858238b16989b7bd45d864
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-15 11:50:04 +0300

    udpated code

---
+++ Lab Solutions/B01-Wednesday/Lab 8 - State Management/state_manage_tut/lib/features/dashboard/presentation/providers/book_provider.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_manage_tut/features/dashboard/data/repositories/book_repo_json_imp.dart';
+import 'package:state_manage_tut/features/dashboard/domain/entities/book.dart';
+
+class BookNotifier extends AsyncNotifier<List<Book>> {
+  final repo = BookRepositoryJsonImpl();
+  @override
+  FutureOr<List<Book>> build() async {
+    final books = await repo.getBooks();
+    return books;
+  }
+
+  Future<void> addBook(Book book) async {
+    // call the repo to add the book
+    // get the updated list of books
+    // update the state
+    final books = state.value ?? [];
+    books.add(book);
+    // state = AsyncData(books);
+    state = AsyncValue.data(books);
+  }
+}
+
+// create the provider
+
+final bookNotifierProvider = AsyncNotifierProvider<BookNotifier, List<Book>>(
+  () => BookNotifier(),
+);
--- Lab Solutions/B01-Wednesday/Lab 8 - State Management/state_manage_tut/lib/features/dashboard/presentation/screens/dashboard.dart
+++ Lab Solutions/B01-Wednesday/Lab 8 - State Management/state_manage_tut/lib/features/dashboard/presentation/screens/dashboard.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:state_manage_tut/core/navigation/app_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state_manage_tut/features/dashboard/domain/entities/book.dart';
+import 'package:state_manage_tut/features/dashboard/presentation/providers/book_provider.dart';
 
-class Dashboard extends StatefulWidget {
+class Dashboard extends ConsumerStatefulWidget {
   const Dashboard({super.key});
 
   @override
-  State<Dashboard> createState() => _DashboardState();
+  ConsumerState<Dashboard> createState() => _DashboardState();
 }
 
-class _DashboardState extends State<Dashboard> {
+// ref
+// read , watch
+//read - one time
+//watch - listen to changes [never use watch outside build method]
+class _DashboardState extends ConsumerState<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    List<String> items = ["Skating 1", "Item 2", "Item 3", "Item4"];
-    return myList(items);
+    final bookList = ref.watch(bookNotifierProvider);
+    // List<String> items = ["Skating 1", "Item 2", "Item 3", "Item4"];
+    return bookList.when(
+      data: (books) => myList(books),
+      error: (error, stackTrace) => Text("Error: $error"),
+      loading: () => CircularProgressIndicator(),
+    );
   }
 
   Widget myList(List<Book> books) {
     return ListView.builder(
       itemBuilder: (context, index) => Card(
         margin: EdgeInsets.all(10),
-        child: ListTile(
-          title: Text("$index: ${books[index].title}"),
-          subtitle: Text(books[index].author),
-          leading: Icon(Icons.book),
-          trailing: Text(books[index].year.toString()),
+        child: InkWell(
+          onTap: () =>
+              ref.read(bookNotifierProvider.notifier).addBook(books[index]),
+          child: ListTile(
+            title: Text("$index: ${books[index].title}"),
+            subtitle: Text(books[index].author),
+            leading: Icon(Icons.book),
+            trailing: Text(books[index].year.toString()),
+          ),
         ),
       ),
       itemCount: books.length,
