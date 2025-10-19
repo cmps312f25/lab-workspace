7f0a84dc2deeb5fa149a7ef78db6a603a2e47e38
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-19 14:57:07 +0300

    updated code

--- Lab Solutions/B51-Sunday/Lab 9 DB/state_management/lib/core/data/database/daos/categories_dao.dart
+++ Lab Solutions/B51-Sunday/Lab 9 DB/state_management/lib/core/data/database/daos/categories_dao.dart
@@ -3,21 +3,30 @@ import 'package:state_management_tut/features/dashboard/domain/entities/category
 
 @dao
 abstract class CategoryDao {
-  Stream<List<Category>> getCategories();
+  @Query("SELECT * FROM categories")
+  Stream<List<Category>> observeCategories();
 
+  @Query("SELECT * FROM categories WHERE id =:id")
   Future<Category?> getCategoryById(int id);
 
+  @insert
   Future<void> addCategory(Category category);
 
+  @update
   Future<void> updateCategory(Category category);
 
+  @delete
   Future<void> deleteCategory(Category category);
 
+  @Insert(onConflict: OnConflictStrategy.replace)
   Future<void> upsertCategory(Category category);
 
+  @Query("SELECT * FROM categories")
   Future<List<Category>> getAllCategories();
 
+  @Insert(onConflict: OnConflictStrategy.replace)
   Future<void> insertCategories(List<Category> categories);
 
+  @Query("DELETE FROM categories")
   Future<void> deleteAllCategories();
 }
