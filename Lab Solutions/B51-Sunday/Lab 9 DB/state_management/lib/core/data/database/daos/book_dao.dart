e93e435c81fcb70d0f4da29cd3c430aa3b32483a
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-19 14:59:40 +0300

    Daos

--- Lab Solutions/B51-Sunday/Lab 9 DB/state_management/lib/core/data/database/daos/book_dao.dart
+++ Lab Solutions/B51-Sunday/Lab 9 DB/state_management/lib/core/data/database/daos/book_dao.dart
@@ -2,21 +2,30 @@ import 'package:floor/floor.dart';
 import 'package:state_management_tut/features/dashboard/domain/entities/book.dart';
 
 abstract class BookDao {
+  @Query("SELECT * FROM books")
   Stream<List<Book>> getBooks();
 
+  @insert
   Future<void> addBook(Book book);
 
+  @delete
   Future<void> deleteBook(Book book);
 
+  @Query("SELECT * FROM books WHERE id =:id")
   Future<Book?> getBookById(int id);
 
+  @update
   Future<void> updateBook(Book book);
 
+  @Insert(onConflict: OnConflictStrategy.replace)
   Future<void> upsertBook(Book book);
 
+  @Insert(onConflict: OnConflictStrategy.replace)
   Future<void> insertBooks(List<Book> books);
 
+  @Query("DELETE FROM books")
   Future<void> deleteAllBooks();
 
+  @Query("SELECT * FROM books WHERE categoryId =:categoryId")
   Future<List<Book?>> getBooksByCategory(int categoryId);
 }
