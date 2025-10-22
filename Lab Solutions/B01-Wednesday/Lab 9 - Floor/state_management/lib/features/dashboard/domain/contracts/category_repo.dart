1f163c9a95531fdc5ebb89aeea79e57dc2e3e21d
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-10-22 10:47:53 +0300

    updated repo

--- Lab Solutions/B01-Wednesday/Lab 9 - Floor/state_management/lib/features/dashboard/domain/contracts/category_repo.dart
+++ Lab Solutions/B01-Wednesday/Lab 9 - Floor/state_management/lib/features/dashboard/domain/contracts/category_repo.dart
@@ -1,7 +1,7 @@
 import '../entities/category.dart';
 
 abstract class CategoryRepository {
-  Stream<List<Category>> getCategories();
+  Stream<List<Category>> observeCategories();
   Future<Category?> getCategoryById(int id);
   Future<void> addCategory(Category category);
   Future<void> updateCategory(Category category);
